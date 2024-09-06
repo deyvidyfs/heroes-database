d7a62fab0cf461e1a3a2b9df349956ac22fca6d1
Deyvidy F.S <deyvidyfs@gmail.com>
2024-09-06 04:56:23 -0300

    Initial Commit

---
+++ HeroesDatabase/HeroesDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesDatabaseApp.swift
+//  HeroesDatabase
+//
+//  Created by Deyvidy Luã F.S on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HeroesDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
